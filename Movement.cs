2aad696eb4a74f08a4c87d19ea4e1f5af6556cab
alisonmunnis <46458925+alisonmunnis@users.noreply.github.com>
2019-02-10 15:30:46 -0500

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Vector3 start;
+    public Vector3 end;
+
+    void Update()
+    {
+        start = new Vector3(93.5f, 0.5f, 7.2f);
+        end = new Vector3(93.5f, 0.5f, -3.11f);
+
+        transform.position = new Vector3(Mathf.PingPong(start, end, Time.time, 3);
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject player;
+    public Text countText;
+    public Text scoreText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
+
+    private Rigidbody rb;
+    private int count;
+    private int score;
+    private int lives;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        score = 0;
+        lives = 3;
+        SetAllText();
+        winText.text = "";
+        loseText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            score = score + 1;
+            SetAllText();
+        }
+       
+         else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            lives = lives - 1; // this removes 1 from the score
+            SetAllText();
+        }
+
+        if (score == 13) {
+
+            transform.position = new Vector3(93.0f, 0.5f, 0.0f); 
+        
+        }
+
+        if (lives == 0)
+        {
+            Destroy(player);
+        }
+    }
+
+    void SetAllText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        scoreText.text = "Score: " + score.ToString();
+        if (score >= 26)
+        {
+            winText.text = "You Win!";
+        }
+
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives == 0)
+        {
+            loseText.text = "LOSER!";
+        }
+    }
+
+}
